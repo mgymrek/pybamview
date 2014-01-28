36d9ac93f584e05e0871e3b4a431166ecb5205fb
mgymrek <mgymrek@mit.edu>
2014-01-28 17:34:40 -0500

    getting rid of javascript table scrolling. attempt to scroll to vaguely the right place

--- pybamview.js
+++ pybamview.js
@@ -26,87 +26,9 @@ function ScrollLeft() {
     currentpos = currentpos - 1;
 }
 
-function ScrollRight() {
-    // If scrolling right goes out of bounds, do nothing
-    if (currentpos == maxpos) {
-	return;
-    }
-    // Set currentpos to invisible
-    var elemsleft = document.getElementsByClassName(chrom + "_" + (currentpos));
-    for (var i = 0; i < elemsleft.length; i++) {
-	elemsleft[i].style.display = "none";
-    }
-    // Set currentpos + NUMCHAR + 1 to visible
-    var elemsright = document.getElementsByClassName(chrom + "_" + (currentpos + NUMCHARS + 1));
-    for (var i = 0; i < elemsright.length; i++) {
-	elemsright[i].style.display = "block";
-    }
-    // Set currentpos to currentpos + 1
-    currentpos = currentpos + 1;
-}
-
-$(document).keydown(function(e){
-    // Scroll left (left arrow)
-    if (e.keyCode == 37) { 
-	ScrollLeft();
-	return true;
-    }
-    // Scroll right (right arrow)
-    if (e.keyCode == 39) {
-	ScrollRight();
-	return true;
-    }
-    // Big scroll left (r)
-    if (e.keyCode == 82) {
-	for (var i = 0; i < 10; i++) {
-	    ScrollLeft();
-	}
-	return true;
-    }
-    // Big scroll right (a)
-    if (e.keyCode == 65) {
-	for (var i = 0; i < 10; i++) {
-	    ScrollRight();
-	}
-	return true;
-    }
-});
-
-// Get the width of the page to know how many bp to display
-function SetPageWidth() {
-    var width = $("#header_wrap").width();
-    var form = document.forms[0];
-    form.elements["width"].value = width;
-    return true;
-}
-
-function UpdateTable() {
-    // Set all table elements to none
-    for (var i = currentpos - NUMCHARS; i < currentpos + NUMCHARS*2; i++) {
-	elems = document.getElementsByClassName(chrom + "_" + i);
-	for (var j = 0; j < elems.length; j++) {
-	    elems[j].style.display = "none";
-	}	
-    }
-    // Set the right ones to visible
-    var form = document.forms[0];
-    for (var i = currentpos; i < currentpos + (form.elements["width"].value/14); i++) {
-	elems = document.getElementsByClassName(chrom + "_" + i);
-	for (var j = 0; j < elems.length; j++) {
-	    elems[j].style.display = "block";
-	}
-    }
-}
-
-$(window).resize(function()
-{
-    SetPageWidth();
-    UpdateTable();
-});
 // Select columns by colgroup
 $(document).ready(function()
 {
-    SetPageWidth();
 $("td, th").hover
     (
      // mouseover
--- pybamview/display_html.py
+++ pybamview/display_html.py
@@ -1,7 +1,7 @@
 import sys
 from constants import *
 
-def GetHeader(bamfiles, region, minpos, maxpos, reffile, samples, numdisplay):
+def GetHeader(bamfiles, region, minpos, maxpos, reffile, samples):
     """
     Get header div fo pybamview
     """
@@ -12,11 +12,10 @@ def GetHeader(bamfiles, region, minpos, maxpos, reffile, samples, numdisplay):
     header_html += "<link rel='shortcut icon' href='/favicon.ico'>"
     header_html += "<style type='text/css'>%s</style>"%open("%s/pybamview.css"%CSS_PREFIX,"r").read()
     header_html += "<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js\"></script>"
-    header_html += "<script language=javascript type='text/javascript'>var NUMSAMPLES=%s;var NUMCHARS=%s;var currentpos=%s;var minpos=%s;var maxpos=%s;var chrom=\"%s\";\n%s</script>"\
-        %(len(samples),numdisplay,position,minpos,maxpos-numdisplay,chrom,open("%s/pybamview.js"%JS_PREFIX,"r").read())
+    header_html += "<script language=javascript type='text/javascript'>var chrom=\"%s\";\n%s</script>"\
+        %(chrom,open("%s/pybamview.js"%JS_PREFIX,"r").read())
     header_html += "<title>PyBamView: %s</title>"%",".join(bamfiles)
     header_html += "</head>"
-    header_html += "<div class='fixedElement'>" # begin fixed
     header_html += "<div class='outer' id='header_wrap'>"
     header_html += "<header class='inner'>"
     header_html += "<h1 id='display_info'>PyBamView</h1>"
@@ -27,7 +26,6 @@ def GetHeader(bamfiles, region, minpos, maxpos, reffile, samples, numdisplay):
     for sample in samples:
         header_html += "<a href='#%s'><font color='white'>%s</font></a> "%(sample, sample)
     header_html += "</div>"
-    header_html += "<div id='param_info'>Keyboard commands: [left],[right] scroll by one, [a](advance),[r](reverse) scroll by 10</div>"
     header_html += "</div></header>"
     return header_html
 
@@ -45,7 +43,6 @@ def GetToolbar(chrom, pos, bamfiles, settings):
     toolbar_html += "<form>"
     for bam in bamfiles:
         toolbar_html += "<input type='hidden' name='bamfiles', value='%s'>"%bam
-    toolbar_html += "<input type='hidden' name='width'>"
     toolbar_html += "Enter region: <input type='text' name='region' value=%s>"%settings["region"]
     toolbar_html += "<input type='submit'>"
     toolbar_html += "<div id='selected'>Selected: </div>"
@@ -53,45 +50,36 @@ def GetToolbar(chrom, pos, bamfiles, settings):
     toolbar_html += "</div>"
     return toolbar_html
 
-def GetReference(reference_string, chrom, pos, positions, numdisplay):
+def GetReference(reference_string, chrom, pos, positions):
     """
     Get HTML to display the reference sequence
     """
-    min_pos = pos
-    max_pos = pos + numdisplay
     reference_html = "<div class='reference' id='reference'>"
     reference_html += "<table>"
     reference_html += "<tr>"
     for i in range(len(reference_string)):
         color = NUC_TO_COLOR.get(reference_string[i], "gray")
-        display = ""
-        if positions[i] < min_pos or positions[i] > max_pos: display = "display:none;"
-        reference_html += "<td class='%s_%s' style='background-color:%s;%s'><font class='ref'><b>%s</b></font></div></td>"%(chrom, positions[i], color, display, reference_string[i])
+        reference_html += "<td class='%s_%s' id='ref%s' style='background-color:%s;'><font class='ref'><b>%s</b></font></td>"%(chrom, positions[i], i, color, reference_string[i])
     reference_html += "</tr>"
     reference_html += "</table></div>"
-    reference_html += "</div>" # end fixed
     return reference_html
 
-def GetAlignment(alignments_by_sample, numcols, chrom, pos, positions, numdisplay):
+def GetAlignment(alignments_by_sample, numcols, chrom, pos, positions):
     """
     Get HTML for the alignment div
     """
-    min_pos = pos
-    max_pos = pos + numdisplay
     aln_html = "<div class='alignments'>"
     for sample in alignments_by_sample:
         aln_html += "<a name='%s'></a>"%sample
-        aln_html += "<div style='background-color:lightgray;' onclick='toggleDiv(\"%s\");'>%s (show/hide)</div>"%(sample, sample)
-        aln_html += "<div id='%s' style:'background-color:red;'>"%sample
+        aln_html += "<div class='sample' onclick='toggleDiv(\"%s\");'>%s (show/hide)</div><div>how did you find me?</div>"%(sample, sample)
+        aln_html += "<div id='%s'>"%sample
         aln_html += "<table>"
         alignment_list = alignments_by_sample[sample]
         for aln in alignment_list:
             aln_html += "<tr>"
             for i in range(len(aln)):
                 color = NUC_TO_COLOR.get(aln[i], "black")
-                display = ""
-                if positions[i] < min_pos or positions[i] > max_pos: display = "display:none;"
-                aln_html += "<td class='%s_%s' style='text-align:center;%s'><font class='read' color='%s'>%s</font></td>"%(chrom, positions[i], display, color, aln[i])
+                aln_html += "<td class='%s_%s' style='text-align:center;'><font class='read' color='%s'>%s</font></td>"%(chrom, positions[i], color, aln[i])
             aln_html += "</tr>"
         aln_html += "</table>"
         aln_html += "</div><br>"
