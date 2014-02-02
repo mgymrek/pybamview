0cd1be2ace6d10a62fb8b8cc47f4bc4f87756232
mgymrek <mgymrek@mit.edu>
2014-02-01 22:52:54 -0500

    don't require the default installation path

--- pybamview/constants.py
+++ pybamview/constants.py
@@ -24,11 +24,6 @@ THE SOFTWARE.
 
 # Global constants used by other files
 
-import sys
-
-PREFIX = "/usr/local/"
-CSS_PREFIX = PREFIX + "/pybamview/css"
-JS_PREFIX = PREFIX + "/pybamview/javascript"
 ENDCHAR = "-"
 GAPCHAR = "."
 DELCHAR = "*"
