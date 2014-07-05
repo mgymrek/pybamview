7b4644491c22b97bfe938ba88352480614745d51
mgymrek <mgymrek@mit.edu>
2014-07-05 09:16:59 -0400

    implementing zoom

--- pybamview/data/javascript/pybamview.js
+++ pybamview/data/javascript/pybamview.js
@@ -1,28 +1,69 @@
+BASE_W = 15;
+BASE_H = 20;
+BASE_FONT = 16;
+
 function IsNuc(x) {
     return (x=="A" || x=="C" || x=="G" || x=="T");
 }
 
-function InHover(i) {
-    // Update color
-    d3.selectAll(".p"+i).style("fill","pink");
+function InHover(i, usefont) {
+    if (usefont) {
+	// Update color
+	d3.selectAll(".p"+i).style("fill","pink");
+    } else {
+	d3.selectAll(".p"+i)
+	    .style("stroke","black")
+	    .style("stroke-width",1);
+    }
     // Update selected box
     var x = document.getElementById("selected");
     x.innerHTML = "Selected: " + chrom + ":" + i;
 }
 
-function OutHover(i) {
-    // Update color
-    d3.selectAll(".p"+i).style("fill","white");
+function OutHover(i, usefont) {
+    if (usefont) {
+	// Update color
+	d3.selectAll(".p"+i).style("fill","white");
+    } else {
+	d3.selectAll(".p"+i).style("stroke-width",0);
+    }
     // Update selected box
     var x = document.getElementById("selected");
     x.innerHTML = "Selected ";
 }
 
+function AlignZoom(zoomlevel) {
+    var frompos = parseInt(region.split("-")[0]);
+    var topos = parseInt(region.split("-")[1]);
+    fromindex=frompos-startpos;
+    toindex=topos-startpos;
+    zoomlevel = parseFloat(zoomlevel);
+    if (zoomlevel < 0) {
+	zoomlevel = -1/zoomlevel;
+    }
+    DrawSnapshot(reference_track, samples, alignBySample, fromindex, toindex, zoomlevel);
+    // TODO - scroll, don't make hardcoded
+    $("#aln").scrollLeft(BASE_W*zoomlevel*250);
+}
 
-function DrawSnapshot(reference_track, samples, alignBySample, fromindex, toindex) {
+function DrawSnapshot(reference_track, samples, alignBySample, fromindex, toindex, zoomlevel) {
+    // Reset
+    document.getElementById("reference").innerHTML = "";
+    var divs = document.getElementsByClassName("sampleAlignment");
+    for (var i = 0; i<divs.length; i++) {
+	divs[i].innerHTML = "";
+    }
     // Set positioning variables
-    gridWidth = 15;
-    gridHeight = 20;
+    usefont = true;
+    gridWidth = BASE_W*zoomlevel;
+    gridHeight = BASE_H*zoomlevel;
+    fontSize = BASE_FONT*zoomlevel;
+    if (gridHeight < 10) {
+	gridHeight = 10;
+    }
+    if (zoomlevel < 1/3) {
+	usefont = false;
+    }
     var numreads = 0;
     for (var i =0; i < samples.length; i++) {
 	numreads += alignBySample[samples[i]].split(";").length;
@@ -61,22 +102,26 @@ function DrawSnapshot(reference_track, samples, alignBySample, fromindex, toinde
 	.attr("width", gridWidth)
 	.attr("height", gridHeight)
 	.attr("id", function(d, i) {return "ref"+i;})
+	.on("mouseover", function(d,i) {InHover(positions[i], usefont);})
+	.on("mouseout", function(d,i) {OutHover(positions[i], usefont);})
 	.style("fill", function(d) {return colors[d];})
-	.style("stroke", "white");
-    RefTrack.append("text")
-	.attr("x", function(d, i) {return i*gridWidth+gridWidth/2; })
-	.attr("y", gridHeight/2)
-	.attr("dy", ".25em")
-	.attr("fill", "white")
-	.on("mouseover", function(d,i) {InHover(positions[i]);})
-	.on("mouseout", function(d,i) {OutHover(positions[i]);})
-	.style("font-family", "Courier")
-	.style("text-anchor", "middle")
-	.text(function(d) {return d;});
-
+	.style("stroke", function(d) {return usefont?"white":colors[d];});
+    if (usefont) {
+	RefTrack.append("text")
+	    .attr("x", function(d, i) {return i*gridWidth+gridWidth/2; })
+	    .attr("y", gridHeight/2)
+	    .attr("dy", ".25em")
+	    .attr("fill", "white")
+	    .on("mouseover", function(d,i) {InHover(positions[i], usefont);})
+	    .on("mouseout", function(d,i) {OutHover(positions[i], usefont);})
+	    .style("font-family", "Courier")
+	    .style("font-size", fontSize)
+	    .style("text-anchor", "middle")
+	    .text(function(d) {return d;});
+    }
     // Draw each sample
     for (var i=0; i < samples.length; i++) {
-	var currentHeight = 0;
+	var currentHeight = 20;
 	// Make div for the sample
 	var samplesvg = d3.select("#"+samples[i]).append("svg:svg")
 	    .attr("width",w)
@@ -95,19 +140,23 @@ function DrawSnapshot(reference_track, samples, alignBySample, fromindex, toinde
 		.attr("width", gridWidth)
 		.attr("height", gridHeight)
 		.attr("class", function(d, i) {return "p"+positions[i];})
+		.on("mouseover", function(d,i) {InHover(positions[i], usefont);})
+		.on("mouseout", function(d,i) {OutHover(positions[i], usefont);})
 		.style("fill", function(d, pos) {return (d.toUpperCase()!=refdata[pos].toUpperCase() &&
 							 IsNuc(refdata[pos].toUpperCase()) && 
-							 IsNuc(d.toUpperCase()))?"yellow":"white";});
-	    SampleTrack.append("text")
-		.text(function(d) {return d;})
-		.attr("x", function(d, pos) {return pos*gridWidth+gridWidth/2;})
-		.attr("y", currentHeight + gridHeight/2)
-		.attr("dy", ".25em")
-		.on("mouseover", function(d,i) {InHover(positions[i]);})
-		.on("mouseout", function(d,i) {OutHover(positions[i]);})
-		.style("font-family", "Courier")
-		.style("text-anchor", "middle")
-		.attr("fill", function(d) {return colors[d];});
+							 IsNuc(d.toUpperCase()))?"yellow":(usefont?"white":colors[d]);});
+	    if (usefont) {
+		SampleTrack.append("text")
+		    .text(function(d) {return d;})
+		    .attr("x", function(d, pos) {return pos*gridWidth+gridWidth/2;})
+		    .attr("y", currentHeight + gridHeight/2)
+		    .on("mouseover", function(d,i) {InHover(positions[i], usefont);})
+		    .on("mouseout", function(d,i) {OutHover(positions[i], usefont);})
+		    .style("font-family", "Courier")
+		    .style("font-size", fontSize)
+		    .style("text-anchor", "middle")
+		    .attr("fill", function(d) {return colors[d];});
+	    }
 	    currentHeight += gridHeight;
 	}
 	// Update position
@@ -141,8 +190,8 @@ var frompos = parseInt(region.split("-")[0]);
 var topos = parseInt(region.split("-")[1]);
 fromindex=frompos-startpos;
 toindex=topos-startpos;
-DrawSnapshot(reference_track, samples, alignBySample, fromindex, toindex);
+DrawSnapshot(reference_track, samples, alignBySample, fromindex, toindex, 1);
 
-var w = 15; // TODO these are hard coded, change that
-$("#aln").scrollLeft(w*250);
+// TODO these are hard coded, change that
+$("#aln").scrollLeft(BASE_W*250);
 });
