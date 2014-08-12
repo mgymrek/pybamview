f5f686aded0ec4e658da3aef48e90fcef522dfb3
mgymrek <mgymrek@mit.edu>
2014-08-12 13:53:23 -0400

    updating version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ MAINTAINER_EMAIL = "mgymrek@mit.edu"
 DOWNLOAD_URL = 'http://github.com/mgymrek/pybamview'
 LICENSE = 'MIT'
 
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 
 setup(name=NAME,
       version=VERSION,
