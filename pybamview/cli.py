e0568139b4d7a67946d1e36e723dfdf27dbcc2e7
Robin Andeer <robin.andeer@gmail.com>
2014-12-12 15:50:42 +0100

    move all pre app creation config to config object

--- pybamview/cli.py
+++ pybamview/cli.py
@@ -32,6 +32,7 @@ import threading
 import webbrowser
 
 from .app import create_app
+from .settings import DefaultConfig
 from .utils import message, ParseTargets, random_ports
 
 
@@ -43,9 +44,17 @@ def parse_args():
     parser.add_option('--ip', help='Host IP. 127.0.0.1 for local host (default). 0.0.0.0 to have the server available externally.', type='string', default="127.0.0.1")
     parser.add_option('--port', help='The port of the webserver. Defaults to 5000.', type='int', default=5000)
     parser.add_option('--targets', help='Bed file with chrom, start, end, and name. Allows you to easily jump to these targets.', type='string')
-    parser.add_option('--buffer', help='How many nucleotides to load into memory. Default: 200. Buffering more allows you to scroll farther. Buffering less is faster.', \
-                            type='int', default=200)
-    parser.add_option("--maxzoom", help="Maximum number of times to be able to zoom out. E.g. --maxzoom 10 allows zooming out up to 10x. Default: 100. Must be between one of 1-10, 50, or 100.", type='int', default=100)
+    parser.add_option('--buffer',
+                      help="How many nucleotides to load into memory. "
+                           "Default: 200. Buffering more allows you to "
+                           "scroll farther. Buffering less is faster.",
+                      type='int', default=DefaultConfig.SETTINGS["NUMCHAR"])
+    parser.add_option("--maxzoom",
+                      help="Maximum number of times to be able to zoom out. "
+                           "E.g. --maxzoom 10 allows zooming out up to 10x. "
+                           "Default: 100. Must be between one of 1-10, 50, "
+                           "or 100.", 
+                      type='int', default=DefaultConfig.SETTINGS["MAXZOOM"])
     parser.add_option('--no-browser', help="Don't automatically open the web browser.", action="store_true")
     parser.add_option('--debug', help='Run PyBamView in Flask debug mode', action="store_true")
 
@@ -63,71 +72,67 @@ def cli():
     if options.bamdir and options.bam:
         message('You must specify only one of --bam or --bamdir', "error")
 
-    app = create_app()
+    host = options.ip
+    default_port = options.port
 
-    app.config["BAM"] = options.bam
-    app.config["BAMDIR"] = options.bamdir
-    app.config["REFFILE"] = REFFILE = options.ref
-    HOST = options.ip
-    PORT = options.port
-    TARGETFILE = options.targets
-    NUMCHAR = options.buffer
-    MAXZOOM = options.maxzoom
-    if MAXZOOM not in range(1,11) + [50, 100]:
-        message("Must set --maxzoom to one of 1-10, 50, or 100", "error")
-    app.debug = options.debug
-    OPEN_BROWSER = (not options.no_browser)
-
-    app.config["BAMFILE_TO_BAMVIEW"] = {}
-    app.config["PORT_RETRIES"] = 50
-    app.config["SETTINGS"] = {
-        "NUMCHAR": NUMCHAR,
-        "MAXZOOM": MAXZOOM,
-        "LOADCHAR": NUMCHAR * MAXZOOM
+    config = DefaultConfig()
+    config.DEBUG = options.debug
+    config.BAM = options.bam
+    config.BAMDIR = options.bamdir
+    config.REFFILE = ref_file = options.ref
+    config.SETTINGS = {
+        "NUMCHAR": options.buffer,
+        "MAXZOOM": options.maxzoom,
+        "LOADCHAR": options.buffer * options.maxzoom
     }
 
+    if options.maxzoom not in range(1,11) + [50, 100]:
+        message("Must set --maxzoom to one of 1-10, 50, or 100", "error")
+    OPEN_BROWSER = (not options.no_browser)
+
     # Load reference
-    if REFFILE is None:
-        REFFILE = "No reference loaded"
-    elif not os.path.exists(REFFILE):
-        message("Could not find reference file %s"%REFFILE, "warning")
-        REFFILE = "Could not find reference file %s"%REFFILE
+    if ref_file is None:
+        config.REFFILE = "No reference loaded"
+    elif not os.path.exists(ref_file):
+        message("Could not find reference file %s" % ref_file, "warning")
+        config.REFFILE = "Could not find reference file %s" % ref_file
     else:
         try:
-            _ = pyfasta.Fasta(REFFILE) # Make sure we can open the fasta file
+            _ = pyfasta.Fasta(ref_file) # Make sure we can open the fasta file
         except:
-            message("Invalid reference fasta file %s"%REFFILE, "warning")
-            REFFILE = "Invalid fasta file %s"%REFFILE
+            message("Invalid reference fasta file %s" % ref_file, "warning")
+            config.REFFILE = "Invalid fasta file %s" % ref_file
 
     # Parse targets
-    if TARGETFILE is not None:
-        if not os.path.exists(TARGETFILE):
-            message("Target file %s does not exist"%TARGETFILE, "warning")
-            TARGETFILE = None
+    target_file = options.targets
+    if target_file is not None:
+        if not os.path.exists(target_file):
+            message("Target file %s does not exist" % target_file, "warning")
         else:
-            app.config['TARGET_LIST'] = ParseTargets(TARGETFILE)
+            config.TARGET_LIST = ParseTargets(target_file)
 
     # Start app
+    app = create_app(config_object=config)
     success = False
-    for port in random_ports(PORT, app.config["PORT_RETRIES"] + 1):
+    for port in random_ports(default_port, app.config["PORT_RETRIES"] + 1):
         try:
             if OPEN_BROWSER:
-                url = "http://%(host)s:%(port)s" % dict(host=HOST, port=port)
+                url = "http://%(host)s:%(port)s" % dict(host=host, port=port)
                 threading.Timer(1.5, lambda: webbrowser.open(url)).start()
-            app.run(host=HOST, port=port)
+            app.run(host=host, port=port)
             success = True
         except webbrowser.Error as e:
-            message("No web browser found: %s."%e, "warning")
+            message("No web browser found: %s." % e, "warning")
         except socket.error as e:
             if e.errno == errno.EADDRINUSE:
-                message("Port %s is already in use. Trying another port"%port, "warning")
+                message("Port %s is already in use. Trying another port" % port, "warning")
                 continue
             elif e.errno in (errno.EACCES, getattr(errno, "WSEACCES", errno.EACCES)):
-                message("Permission denied to listen on port %s. Trying another port"%port, "warning")
+                message("Permission denied to listen on port %s. Trying another port" % port, "warning")
                 continue
             else: raise
         except OverflowError:
-            message("Invalid port specified (%s)."%port, "error")
+            message("Invalid port specified (%s)." % port, "error")
         else: break
     if not success:
         message("PyBamView could not find an available port. Quitting", "error")
